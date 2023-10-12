5adaee05abd9f61eb0ddd62e1532ea970d505247
RodrigoPinto22 <131172504+RodrigoPinto22@users.noreply.github.com>
2023-10-12 21:14:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
+let height = 0;
+let width = 0;
+
 function getSize() {
-    boardSize = document.getElementById("bSize").value;
-    console.log(boardSize)
+    const boardSize = document.getElementById("bSize").value;
+    const dimensions = boardSize.split("x"); // Assuming boardSize is in the format "height x width"
+    
+    height = parseInt(dimensions[0], 10);
+    width = parseInt(dimensions[1], 10);
+    createBoard();
 }
 
+function createBoard() {
+    const board = document.querySelector(".board");
+    
+    board.innerHTML = "";
 
-
-function handleClick() {
-    const boardElement = document.querySelector(".board");
-
-    for (let i = 0; i < 6; i++) {
-        const newDiv = document.createElement("div");
-
-        if(i%2 == 0) {
-            newDiv.style.width = "50px";
-            newDiv.style.height = "50px";
-            newDiv.style.backgroundColor = "black";
+    for (let i = 0; i < height; i++) {
+        const row = document.createElement("tr");
+        board.appendChild(row);
+        for (let j = 0; j < width; j++) {
+            const cell = document.createElement("td");
+            cell.style.width = "100px";
+            cell.style.height = "100px";
+            cell.style.backgroundColor = (j + i) % 2 === 0 ? "white" : "black";
+            row.appendChild(cell);
         }
-        else{
-            newDiv.style.width = "50px";
-            newDiv.style.height = "50px";
-            newDiv.style.backgroundColor = "white";
-        }
-        boardElement.appendChild(newDiv);
     }
-   
+}
 
-
+function startGame() {
+    alert("teste")
 }
