f3ccee99027ec121fb20af6284f0100f9250af99
RodrigoPinto22 <131172504+RodrigoPinto22@users.noreply.github.com>
2023-12-04 16:44:54 +0000

    Update server.js

--- projeto 1.0/server.js
+++ projeto 1.0/server.js
@@ -1,39 +1,29 @@
-function registerPlayer(username, password) {
-    // Server endpoint for registration
-    const endpoint = 'http://twserver.alunos.dcc.fc.up.pt:8008/register';
+const LINK = "http://twserver.alunos.dcc.fc.up.pt:8008"
 
-    // Data to be sent in the request body
-    const data = {
-        username: username,
-        password: password
-    };
+function login() {
+    const nick = document.getElementById("username");
+    const password = document.getElementById("password");
 
-    // Fetch options for the POST request
-    const options = {
+    fetch(LINK + "/register",{
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-
-    // Make the fetch request
-    fetch(endpoint, options)
-        .then(response => {
-            // Check if the response status is OK (200-299)
-            if (!response.ok) {
-                throw new Error(`Registration failed with status: ${response.status}`);
-            }
-            // Registration successful
-            console.log('Player registered successfully!');
-            return response.json(); // If the server sends JSON in the response, otherwise, you can use response.text()
-        })
-        .then(data => {
-            // Process the data if needed
-            console.log('Server response:', data);
-        })
-        .catch(error => {
-            // Handle errors
-            console.error('Error during registration:', error.message);
-        });
+        body: JSON.stringify( { nick, password } )  
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((error) => console.error('Error during fetch:', error));
 }
+
+function join() {
+    const group = 
+    const nick = document.getElementById("username");
+    const password = document.getElementById("password");
+
+    fetch(LINK + "/register",{
+        method: 'POST',
+        body: JSON.stringify( { nick, password } )  
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((error) => console.error('Error during fetch:', error));
+}
+{"group": 99, "nick": "zp", "password": "secret", "size": { "rows": 6, "columns": 5 } }
